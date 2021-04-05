7d2927808b4cc97e50cf1e886172aa7e73a5b4e2
Nigel Marshal <nigelmarshal@gmail.com>
2021-04-05 20:24:27 +0400

    feat(product-card): Switched to material UI. Created base card for testing

--- src/Components/Products/Product/Product.js
+++ src/Components/Products/Product/Product.js
@@ -1,14 +1,34 @@
 import React from 'react'
-import {Col} from 'react-bootstrap/';
+import {Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
+import { AddShoppingCart} from '@material-ui/icons';
+import useStyles from './styles';
 
 const Product = ({product}) => {
-    return (
-        <div>
-            <Col title={product.name}>
-                <p>{product.name}</p>
-            </Col>
+    const classes = useStyles();
 
-        </div>
+    return (
+       <Card className={classes.root}>
+           <CardMedia className={classes.media} image='https://dummyimage.com/600x400/000/fff' title={product.name}/>
+           <CardContent>
+               <div>
+                   <Typography gutterBottom variant="h5" component="h2">
+                       {product.name}
+                   </Typography>
+                   <Typography variant="body2" color="textSecondary" component="p">
+                        {product.description}
+                    </Typography>
+                    <Typography>
+                       {product.price}
+                    </Typography>
+               </div>
+             
+           </CardContent>
+           <CardActions>
+               <IconButton aria-label="Add item to cart">
+                   <AddShoppingCart />
+               </IconButton>
+           </CardActions>
+       </Card>
     )
 }
 
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {Container, Row, Col} from 'react-bootstrap/';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import {Grid} from '@material-ui/core';
 
 import Product from './Product/Product'
 
@@ -11,17 +10,17 @@ const products = [
 
 const Products = () =>{
     return (  
-        <Container className="text-center">
-            <Row>  
-                {products.map((product) =>(
-                    <Col item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                       <Product product={product} /> 
-                    </Col>
-                ))}
-            </Row>
-        </Container>
+        <main>
+            <Grid container justify="center" spacing={6}>
+            {products.map((product) => (
+                <Grid item key={product.id} >
+                    <Product product={product} />
+                </Grid>
+            ))}
+            </Grid>
+        </main>
     )
   
 }
 
-export default Products
+export default Products;
