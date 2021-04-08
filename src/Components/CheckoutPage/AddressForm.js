f01a0f6da5503d9a68bbb9a7f469eca2ceb910a8
Nigel Marshal <nigelmarshal@gmail.com>
2021-04-08 04:13:45 +0400

    fix(address): Fixed email validation issue

--- src/Components/CheckoutPage/AddressForm.js
+++ src/Components/CheckoutPage/AddressForm.js
@@ -2,26 +2,26 @@ import React from 'react'
 import { Typography, Grid, Button } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
 
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import FormInputField from './CustomInputField';
 
 
-const AddressForm = ({checkoutToken, next}) => {
+const AddressForm = ({ checkoutToken, next }) => {
     const methods = useForm();
     return (
         <React.Fragment>
             <Typography variant="h5" gutterBottom>Address</Typography>
             <FormProvider {...methods}>
-                <form onSubmit={methods.handleSubmit((data) => next({ data }) )}>
+                <form onSubmit={methods.handleSubmit((data) => next({ data }))}>
                     <Grid container spacing={3}>
                         <FormInputField required name='firstName' label='First Name' />
                         <FormInputField required name='lastName' label='Last Name' />
                         <FormInputField required name='address' label='Address' />
                         <FormInputField required name='phoneNumber' label='Phone Number' />
-                        <FormInputField required name='emailAddress' label='Email' />
+                        <FormInputField required name='emailAddress' label='Email' type='email' />
                     </Grid>
                     <br />
-                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                         <Button component={Link} to="/checkoutcart" variant="outlined">Back to Cart</Button>
                         <Button type="submit" variant="contained" color="primary">Next</Button>
                     </div>
--- src/Components/CheckoutPage/CustomInputField.js
+++ src/Components/CheckoutPage/CustomInputField.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import {Grid, TextField} from '@material-ui/core';
-import {Controller, useFormContext} from 'react-hook-form';
+import { Grid, TextField } from '@material-ui/core';
+import { Controller, useFormContext } from 'react-hook-form';
 
-function FormInputField ({name, label, required}) {
+function FormInputField({ name, label, required, type }) {
     const { control } = useFormContext();
-    
+
     return (
-            <Grid item xs={12} sm={6}>
-                <Controller 
-                    defaultValue=""
-                    as={TextField} 
-                    control={control}
-                    fullWidth
-                    name={name}
-                    label={label}
-                    required={required}
-                />
-            </Grid>
+        <Grid item xs={12} sm={6}>
+            <Controller
+                defaultValue=""
+                as={TextField}
+                control={control}
+                fullWidth
+                name={name}
+                label={label}
+                required={required}
+                type={type}
+            />
+        </Grid>
     );
 }
 
